8d0dc743b69ed8777d2eb6c200d285d3a28aedae
Morten Nilsen <morten@runsafe.no>
2014-05-01 16:21:08 +0200

    Make it all pretty with some bootstrap

--- templates/feeds.php
+++ templates/feeds.php
@@ -3,17 +3,32 @@
 		echo 'There are no feeds';
 	else
 	{
+?>
+<div class="row">
+	<ul class="list-group">
+<?php
 		foreach($feeds as $feed)
 		{
-			printf(
-				'%2$s: %1$s [%4$d/%5$d/%6$d] <a href="/edit.php?name=%3$s">Edit</a> <a href="/?deactivate=%3$s">Deactivate</a><br>',
-				$feed->uri,
-				$feed->name,
-				urlencode($feed->name),
-				$statistics[$feed->name]->new,
-				$statistics[$feed->name]->added,
-				$statistics[$feed->name]->skipped
-			);
+?>
+		<li class="list-group-item">
+			<div class="row">
+				<div class="col-md-8">
+					<?php echo $feed->name; ?>
+					[<?php echo $statistics[$feed->name]->new . '/' . $statistics[$feed->name]->added . '/' . $statistics[$feed->name]->skipped; ?>]
+					<br />
+					<?php echo $feed->uri; ?>
+				</div>
+				<div class="col-md-4">
+					<a class="btn btn-primary" href="/edit.php?name=<?php echo urlencode($feed->name); ?>">Edit</a>
+					<a class="btn btn-danger" href="/?deactivate=<?php echo urlencode($feed->name); ?>">Deactivate</a>
+				</div>
+			</div>
+		</li>
+<?php
 		}
+?>
+	</ul>
+</div>
+<?php
 	}
 ?>
--- templates/site.php
+++ templates/site.php
@@ -1,15 +1,43 @@
 <!doctype html>
-<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="device-width, initial-scale=1">
 		<title><?php echo $title; ?></title>
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
 	<body>
-		<p>
-			<a href="/">Feeds</a>
-			<a href="/new.php">New</a>
-			<a href="/torrent.php">Torrents</a>
-			<a href="/watch.php">Watchlist</a>
-		</p>
-		<?php echo $content; ?>
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="/">Feeds</a></li>
+						<li><a href="/new.php">New</a></li>
+						<li><a href="/torrent.php">Torrents</a></li>
+						<li><a href="/watch.php">Watchlist</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<br /><br /><br />
+			<p><?php echo $content; ?></p>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 	</body>
 </html>
