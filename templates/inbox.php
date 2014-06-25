ed98d26b3b665049d669380c45a5c42b89bb994c
Morten Nilsen <morten@runsafe.no>
2014-06-25 07:23:29 +0200

    Make the inbox match the watchlist in design

--- templates/inbox.php
+++ templates/inbox.php
@@ -2,12 +2,23 @@
 	if(count($torrents) == 0)
 		echo 'There are no new torrents';
 	else
+	{
+?>
+<ul class="list-group history">
+<?php
 		foreach($torrents as $torrent)
 		{
-			printf(
-				'%1$s - [<a href="?start=%2$s">Start</a>] [<a href="?skip=%2$s">Skip</a>]<br>',
-				$torrent->title,
-				urlencode($torrent->torrent)
-			);
+?>
+	<li class="list-group-item">
+		<div class="row">
+			<div class="col-md-10"><?php echo $torrent->title; ?></div>
+			<div class="col-md-2">
+				<a class="btn btn-xs btn-primary" href="?start=<?php echo urlencode($torrent->torrent); ?>">Start</a>
+				<a class="btn btn-xs btn-success" href="?skip=<?php echo urlencode($torrent->torrent); ?>">Skip</a>
+			</div>
+		</div>
+	</li>
+<?php
 		}
 ?>
+</ul>
