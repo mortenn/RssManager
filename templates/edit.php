a88ed4772ff380c1be8e6f668399c9c59b10adc2
Morten Nilsen <morten@runsafe.no>
2014-07-03 18:42:34 +0200

    Post to the correct URL

--- templates/edit.php
+++ templates/edit.php
@@ -1,10 +1,10 @@
 Feed for <?php echo $feed->name; ?>:<br>
-<form method="post" action="/edit.php">
+<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
 	<input type="hidden" name="name" value="<?php echo $feed->name; ?>">
 	<input type="hidden" name="term" value="<?php echo $feed->term; ?>">
 	<input type="submit" value="Save">
 </form>
-<form method="get" action="/edit.php">
+<form method="get" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
 	<input type="hidden" name="name" value="<?php echo $feed->name; ?>">
 	<input type="text" name="term" value="<?php echo $feed->term; ?>" size="50">
 	<input type="submit" value="Search">
