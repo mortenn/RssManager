6c3db45d00c79b5e5d36d135cf2ea477869f52a1
Morten Nilsen <morten@runsafe.no>
2014-10-15 00:46:04 +0200

    Tweak listing

--- dal/TorrentTable.php
+++ dal/TorrentTable.php
@@ -10,7 +10,7 @@
 			$this->getByFeed = $this->db->prepare('SELECT * FROM `torrents` WHERE `feed`=:feed');
 			$this->getByStatus = $this->db->prepare('SELECT * FROM `torrents` WHERE `status`=:status');
 			$this->getWatchList = $this->db->prepare('SELECT * FROM `torrents` WHERE (`watched` IS NULL OR `watched` = 0) AND `status`='.TORRENT_STATUS_ADDED.' ORDER BY `added`');
-			$this->getWatchedList = $this->db->prepare('SELECT * FROM `torrents` WHERE `watched`=1 ORDER BY `added` DESC LIMIT 2');
+			$this->getWatchedList = $this->db->prepare('SELECT * FROM `torrents` WHERE `watched`=1 ORDER BY `modified` DESC, `added` DESC LIMIT 2');
 			$this->setWatched = $this->db->prepare('UPDATE `torrents` SET `watched`=1 WHERE `torrent`=:torrent');
 			$this->add = $this->db->prepare('INSERT INTO `torrents` (`feed`,`torrent`,`title`,`status`,`added`) VALUES (:feed,:torrent,:title,:status,NOW())');
 			$this->setStatus = $this->db->prepare('UPDATE `torrents` SET `status`=:status WHERE `torrent`=:torrent');
@@ -64,7 +64,7 @@ GROUP BY `feed`');
 
 		public function getVersion()
 		{
-			return 4;
+			return 5;
 		}
 
 		public function getQueries()
@@ -80,7 +80,8 @@ CREATE TABLE `torrents` (
 				),
 				2 => array('ALTER TABLE `torrents` ADD COLUMN `title` VARCHAR(100)'),
 				3 => array('ALTER TABLE `torrents` ADD COLUMN `watched` BOOLEAN'),
-				4 => array('ALTER TABLE `torrents` ADD COLUMN `added` DATETIME')
+				4 => array('ALTER TABLE `torrents` ADD COLUMN `added` DATETIME'),
+				5 => array('ALTER TABLE `torrents` ADD COLUMN `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP')
 			);
 		}
 	}
