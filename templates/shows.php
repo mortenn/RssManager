ea14cc9de6387cede0be931a52eb0940b048c52a
Morten Nilsen <morten@runsafe.no>
2015-05-06 00:43:33 +0200

    Moved torrent item into separate template

--- templates/shows.php
+++ templates/shows.php
@@ -25,6 +25,7 @@
 		echo 'There is nothing to watch';
 	else
 	{
+		$template = new KW_Template('torrent');
 		$shows = array();
 		foreach($list as $file)
 		{
@@ -50,50 +51,8 @@
 		{
 			if(!is_array($files))
 			{
-				$files->status();
-				$file = $files;
-				$real = false;
-				if(is_dir(TARGET.$file->feed.'/'.$file->title))
-				{
-					$real = array();
-					$dir = opendir(TARGET.$file->feed.'/'.$file->title);
-					while($e = readdir($dir))
-						if(is_file(TARGET.$file->feed.'/'.$file->title.'/'.$e))
-							$real[] = $e;
-					sort($real);
-				}
-				$status = $file->status();
-				if($status != null)
-					$done = isset($status->haveValid) && $status->haveValid == $status->totalSize;
-				else
-					$done = true;
-?>
-	<li class="list-group-item">
-		<div class="row">
-			<div class="col-md-10 show-head"><?php echo $file->title; ?></div>
-			<div class="col-md-2">
-				<a class="btn btn-xs btn-primary<?php if($real || !$done) echo ' disabled'; ?>" href="play?name=<?php echo urlencode($file->torrent); ?>">Play</a>
-				<a class="btn btn-xs btn-success<?php if(!$done) echo ' disabled'; ?>" href="watched?name=<?php echo urlencode($file->torrent); ?>">Done</a>
-			</div>
-<?php
-				if($real)
-				{
-					foreach($real as $content)
-					{
-?>
-		</div>
-		<div class="row subfile">
-			<div class="col-md-10"><?php echo $content; ?></div>
-			<div class="col-md-2">
-				<a class="btn btn-xs btn-primary<?php if(preg_match('/.part$/', $content)) echo ' disabled'; ?>" href="play?name=<?php echo urlencode($file->torrent); ?>&file=<?php echo urlencode($content); ?>">Play</a>
-			</div>
-<?php
-					}
-				}
-?>
-		</div>
-	</li>
-<?php
+				$template->torrent = $files;
+				echo $template;
 				continue;
 			}
 ?>
@@ -107,48 +66,8 @@
 <?php
 			foreach($files as $file)
 			{
-				$real = false;
-				if(is_dir(TARGET.$file->feed.'/'.$file->title))
-				{
-					$real = array();
-					$dir = opendir(TARGET.$file->feed.'/'.$file->title);
-					while($e = readdir($dir))
-						if(is_file(TARGET.$file->feed.'/'.$file->title.'/'.$e))
-							$real[] = $e;
-					sort($real);
-				}
-				$status = $file->status();
-				if($status != null)
-					$done = isset($status->haveValid) && $status->haveValid == $status->totalSize;
-				else
-					$done = true;
-?>
-	<li class="list-group-item">
-		<div class="row">
-			<div class="col-md-10"><?php echo $file->title; ?></div>
-			<div class="col-md-2">
-				<a class="btn btn-xs btn-primary<?php if($real || !$done) echo ' disabled'; ?>" href="play?name=<?php echo urlencode($file->torrent); ?>">Play</a>
-				<a class="btn btn-xs btn-success<?php if(!$done) echo ' disabled'; ?>" href="watched?name=<?php echo urlencode($file->torrent); ?>">Done</a>
-			</div>
-<?php
-				if($real)
-				{
-					foreach($real as $content)
-					{
-?>
-		</div>
-		<div class="row subfile">
-			<div class="col-md-10"><?php echo $content; ?></div>
-			<div class="col-md-2">
-				<a class="btn btn-xs btn-primary<?php if(preg_match('/.part$/', $content)) echo ' disabled'; ?>" href="play?name=<?php echo urlencode($file->torrent); ?>&file=<?php echo urlencode($content); ?>">Play</a>
-			</div>
-<?php
-					}
-				}
-?>
-		</div>
-	</li>
-<?php
+				$template->torrent = $file;
+				echo $template;
 			}
 ?>
 				</ul>
---
+++ templates/torrent.php
@@ -0,0 +1,34 @@
+<?php
+	$targetfile = $torrent->locateTarget();
+	$real = $torrent->getSubfiles();
+	$status = $torrent->status();
+	if($status != null)
+		$done = isset($status->haveValid) && $status->haveValid == $status->totalSize;
+	else
+		$done = true;
+?>
+<li class="list-group-item">
+	<div class="row">
+		<div class="col-md-10"><?php echo $targetfile; ?></div>
+		<div class="col-md-2">
+			<a class="btn btn-xs btn-primary<?php if($real || !$done) echo ' disabled'; ?>" href="play?name=<?php echo urlencode($torrent->torrent); ?>">Play</a>
+			<a class="btn btn-xs btn-success<?php if(!$done) echo ' disabled'; ?>" href="watched?name=<?php echo urlencode($torrent->torrent); ?>">Done</a>
+		</div>
+<?php
+	if($real)
+	{
+		foreach($real as $content)
+		{
+?>
+	</div>
+	<div class="row subfile">
+		<div class="col-md-10"><?php echo $content; ?></div>
+		<div class="col-md-2">
+			<a class="btn btn-xs btn-primary<?php if(preg_match('/.part$/', $content)) echo ' disabled'; ?>" href="play?name=<?php echo urlencode($torrent->torrent); ?>&file=<?php echo urlencode($content); ?>">Play</a>
+		</div>
+<?php
+		}
+	}
+?>
+	</div>
+</li>
