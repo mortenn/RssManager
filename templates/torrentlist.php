d91e695a045facc9218fe6fa86d495af1e470d44
Morten Nilsen <morten@runsafe.no>
2015-07-16 01:40:05 +0200

    Hide torrents that cannot be played from feed list

--- templates/torrentlist.php
+++ templates/torrentlist.php
@@ -1,19 +1,38 @@
 <ul>
 <?php
 	$template = new KW_Template('torrent');
+	$valid = 0;
 	foreach($torrents as $torrent)
 	{
-		$template->torrent = $torrent;
-		echo $template;
+		if($torrent->validTarget())
+		{
+			if($valid == 0) echo '<ul>';
+			$valid++;
+			$template->torrent = $torrent;
+			echo $template;
+		}
 	}
+	if($valid)
+	{
 ?>
 	<li class="list-group-item">
 		<div class="row">
-			<div class="col-md-10">&nbsp;</div>
-			<div class="col-md-2">
-				<a class="btn btn-xs btn-primary" href="play?feed=<?php echo $torrent->feed; ?>&done=0">Play new</a>
-				<a class="btn btn-xs btn-primary" href="play?feed=<?php echo $torrent->feed; ?>&done=1">Play all</a>
+			<div class="col-md-9">&nbsp;</div>
+			<div class="col-md-3">
+				<a class="btn btn-xs btn-primary" style="width:50px" href="play?feed=<?php echo $torrent->feed; ?>&done=0">P.New</a>
+				<a class="btn btn-xs btn-primary" style="width:50px"href="play?feed=<?php echo $torrent->feed; ?>&done=1">P.All</a>
+				<a class="btn btn-xs btn-success" style="width:50px"href="watched?feed=<?php echo $torrent->feed; ?>">D.All</a>
 			</div>
 		</div>
 	</li>
-</ul>
+<?php
+	}
+	if($valid)
+		echo '</ul>';
+	else
+	{
+?>
+<h3>No files found for this feed, maybe they were moved or deleted?</h3>
+<?php
+	}
+?>
